5b973b2602f5814e1ad2bab53143b25ea30ab952
Esther Kang <eskang87@gmail.com>
2022-01-26 12:42:55 -0800

    commit tree ad8b26529c2e180784b57c1b9104727907bfd19c

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+const pi = Math.PI;
+
+class Circle {
+  constructor(radius){
+    this.radius = radius
+  };
+  
+  get diameter() {
+    return this.radius * 2
+  };
+  get circumference() {
+    return pi * (this.radius * 2)
+  };
+  get area() {
+    return pi * (this.radius * this.radius)
+  };
+
+  set diameter(diameter){
+    this.radius = diameter / 2
+  };
+  set circumference(circum) {
+    this.radius = circum / (2 * pi)
+  };
+  set area(area){
+    this.radius = Math.sqrt(area / pi)
+  };
+
+}
+
+/* 
+PEMDAS
+Diameter = radius • 2
+Circumference = π • diameter
+Area = π • radius2
+*/
